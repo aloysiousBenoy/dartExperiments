967bcee54cb6d443cd391f35a2f257565bcab6a6
Aloysious Benoy <aloysiousbenoy@gmail.com>
2020-10-29 00:37:36 +0530

    custom cstor

--- Dino.dart
+++ Dino.dart
@@ -4,10 +4,17 @@ class Dino {
   String name;
   String type;
   String specialAbility;
-  Dino(this.name, this.age, this.type, this.specialAbility);
-  printDino() {
-    print("Dino name : $name");
-    print("Age : $age");
+  Dino(String a, String b, int days) {
+    this.age = (days / 365).round();
+    this.name = "$a $b";
+    this.type = a;
+    this.specialAbility = b;
+  }
+  printDino(String name, int age) {
+    print("Dino pseudo name : $name");
+    print("Dino real name : ${this.name}");
+    print("Age pseudo: $age");
+    print("Dino real age : ${this.age}");
     print("Type of dino : $type");
     print("Special ability : $specialAbility");
   }
--- hello.dart
+++ hello.dart
@@ -1,8 +1,8 @@
 import './Dino.dart';
 
 main(List<String> args) {
-  Dino dino1 = Dino("Dino orangutan", 5, "Velociraptor", "Blaah");
-  dino1.printDino();
+  Dino dino1 = Dino("blaah", "dino", 750);
+  dino1.printDino("hello", 15);
   // fightDino(
   //     dino1: Dino("Dino orangootan", 23, "velociraptor",
   //         "Every ability you can ever think of"),
@@ -10,9 +10,9 @@ main(List<String> args) {
   //         "Dino chimp", 21, "T-rex", "Every ability you can ever think of"));
 }
 
-createDino(String name, int age, String type, String ability) {
-  return Dino(name, age, type, ability);
-}
+// createDino(String name, int age, String type, String ability) {
+//   return Dino(name, age, type, ability);
+// }
 
 fightDino({Dino dino1, Dino dino2}) {
   if (dino1.type == dino2.type) {
